0a174ddbe448ce06506f5f18f24d0b28ac879b72
bahaaCs <bahaaaboukhalil@gmail.com>
2024-10-26 19:46:37 +0300

    Preparing logic with EventListener

--- scripts/game.js
+++ scripts/game.js
@@ -1,12 +1,9 @@
 
+var letters = document.querySelectorAll('.letter')
 
-function test(){
-    console.log("test")
+function typing_letters(){
+    console.log(letters[0].textContent)
 }
-var letters = document.getElementsByClassName("letters")[0].innerHTML
-// letters.addEventListener("click",test)
 
-console.log(letters)
+letters[0].addEventListener("click",typing_letters)
 
-var value = document.querySelectorAll('.letter')[0].textContent
-console.log(value);
