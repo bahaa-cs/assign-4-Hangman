c733681dcb03525cc0da91c7a3a2ff230706fdfc
bahaaCs <bahaaaboukhalil@gmail.com>
2024-10-27 00:36:34 +0200

    Edit: display_answer function | hang sction | done

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,9 @@
 
 
-
+var hang_html=[];
 function display_answer(elements,iteration,element_id,word,hangman_id){
     var letter = elements[iteration].textContent
-    var hang_html=[];
+    
     if (word.includes(letter)){
 
         var result = document.getElementById(element_id).innerHTML.split(' ')
@@ -21,31 +21,35 @@ function display_answer(elements,iteration,element_id,word,hangman_id){
         var head = "<img src='./assets/head.svg' class='head' />"
         var body = "<img src='./assets/body.svg' class='body' />"
         var left_hand = "<img src='./assets/left-hand.svg' class='left-hand' />"
-        var left_leg = "<img src='./assets/left_leg.svg' class='left_leg' />"
-        var right_hand = "<img src='./assets/right_hand.svg' class='right_hand' />"
-        var right_leg = "<img src='./assets/right_leg.svg' class='right_leg' />"
+        var left_leg = "<img src='./assets/left-leg.svg' class='left-leg' />"
+        var right_hand = "<img src='./assets/right-hand.svg' class='right-hand' />"
+        var right_leg = "<img src='./assets/right-leg.svg' class='right-leg' />"
         
-        if (!hang_html.includes(head)){
+        if (!hang_html.includes("head")){
             document.getElementById(hangman_id).innerHTML+=head;
             hang_html.push("head")}
-        else if(!hang_html.includes(body)){
+        else if(!hang_html.includes("body")){
             document.getElementById(hangman_id).innerHTML+=body;
             hang_html.push("body")}
-        else if (!hang_html.includes(left_leg)){
-            document.getElementById(hangman_id).innerHTML+=left_leg;
-            hang_html.push("left_leg")}
-        else if (!hang_html.includes(right_leg)){
-            document.getElementById(hangman_id).innerHTML+=right_leg;
-            hang_html.push("right_leg")}
-        else if (!hang_html.includes(left_hand)){
+        else if (!hang_html.includes("left_hand")){
             document.getElementById(hangman_id).innerHTML+=left_hand;
             hang_html.push("left_hand")}
-        else if (!hang_html.includes(right_hand)){
+        else if (!hang_html.includes("right_hand")){
             document.getElementById(hangman_id).innerHTML+=right_hand;
             hang_html.push("right_hand")}
+        else if (!hang_html.includes("left_leg")){
+            document.getElementById(hangman_id).innerHTML+=left_leg;
+            hang_html.push("left_leg")}
+        else if (!hang_html.includes("right_leg")){
+            document.getElementById(hangman_id).innerHTML+=right_leg;
+            hang_html.push("right_leg")}
+
+
+
     }
     
 }
+
 
 function word_generator(){
     var words=["ENGINEER", "ART", "FLEXBOX", "PRESSURE", "VISION","DISCIPLINE","LOGIC","DEADLINES"]
