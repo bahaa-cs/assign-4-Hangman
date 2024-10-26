9ce05fb0bf46c2e6bd3b3f496b34dd80cb4ed010
bahaaCs <bahaaaboukhalil@gmail.com>
2024-10-26 19:29:15 +0300

    Testing: Query Selector All

---
+++ scripts/game.js
@@ -0,0 +1,12 @@
+
+
+function test(){
+    console.log("test")
+}
+var letters = document.getElementsByClassName("letters")[0].innerHTML
+// letters.addEventListener("click",test)
+
+console.log(letters)
+
+var value = document.querySelectorAll('.letter').values
+console.log(value);
