8931dfab7669c89db36bdb3f5979a66fe473749e
bahaaCs <bahaaaboukhalil@gmail.com>
2024-10-26 23:48:08 +0200

    Edit: display_answer function | hang section

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,7 @@
 
-function display_answer(elements,iteration,element_id,word){
+
+
+function display_answer(elements,iteration,element_id,word,hangman_id){
     var letter = elements[iteration].textContent
     
     if (word.includes(letter)){
@@ -11,6 +13,28 @@ function display_answer(elements,iteration,element_id,word){
             }
         }
         document.getElementById(element_id).innerHTML = result.join(' ')
+
+    }
+    else{
+        var hang_html=document.getElementById(hangman_id).innerHTML
+        var head = "<img src='./assets/head.svg' class='head' />"
+        var body = "<img src='./assets/body.svg' class='body' />"
+        var left_hand = "<img src='./assets/left-hand.svg' class='left-hand' />"
+        var left_leg = "<img src='./assets/left_leg.svg' class='left_leg' />"
+        var right_hand = "<img src='./assets/right_hand.svg' class='right_hand' />"
+        var right_leg = "<img src='./assets/right_leg.svg' class='right_leg' />"
+        if (!hang_html.includes(head))
+            document.getElementById(hangman_id).innerHTML+=head;
+        else if(!hang_html.includes(body))
+            document.getElementById(hangman_id).innerHTML+=body;
+        else if (!hang_html.includes(left_leg))
+            document.getElementById(hangman_id).innerHTML+=left_leg;
+        else if (!hang_html.includes(right_leg))
+            document.getElementById(hangman_id).innerHTML+=right_leg;
+        else if (!hang_html.includes(left_hand))
+            document.getElementById(hangman_id).innerHTML+=left_hand;
+        else if (!hang_html.includes(right_hand))
+            document.getElementById(hangman_id).innerHTML+=right_hand;
     }
     
 }
@@ -27,8 +51,13 @@ var letters = document.querySelectorAll('.letter')
 
 
 for(let i=0;i<letters.length;i++){
-    letters[i].addEventListener("click",function(){display_answer(letters,i,"answer-section",word)})
+    letters[i].addEventListener("click",function(){display_answer(letters,i,"answer-section",word,"hang")})
 }
+
+// if(!document.getElementById(answer-section).innerHTML.includes("_")){
+//     alert("You Won!")
+//     location.reload()
+// }
 
 
 
