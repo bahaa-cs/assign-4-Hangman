bbbf6dc56cd1ea0dbbca678cee6477e817c38219
bahaaCs <bahaaaboukhalil@gmail.com>
2024-10-26 21:02:44 +0300

    Create: function word_generator

--- scripts/game.js
+++ scripts/game.js
@@ -8,3 +8,14 @@ var letters = document.querySelectorAll('.letter')
 for(let i=0;i<letters.length;i++){
     letters[i].addEventListener("click",function(){typing_letters(letters,i)})
 }
+
+function word_generator(){
+    var words=["Engineer", "Art", "FlexBox", "Pressure", "Vision"]
+    var index=Math.floor(Math.random() * words.length)
+    return words[index]
+}
+
+console.log(word_generator())
+
+
+// document.getElementById("answer-section").innerHTML = "-".repeat(2);
